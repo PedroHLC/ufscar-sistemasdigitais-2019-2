6df6556834ec478c1c693f187f082ce81acd18d8
PedroHLC <root@pedrohlc.com>
2019-11-07 15:53:19 -0300

    New statemachine + edge detection state

--- top.sv
+++ top.sv
@@ -1,6 +1,6 @@
 module top
 	( input CLOCK_50
-	, input [9:5] SW
+	, input [9:7] SW
 	, output [9:0] LEDR
 	, output [7:0] VGA_R, VGA_G, VGA_B
 	, output VGA_HS, VGA_VS
@@ -8,11 +8,9 @@ module top
 	);
 	
 	// Define botoes
-	wire reset;
-	assign reset = SW[9];
 	wire [3:0] proc_what;
-	//assign proc_what = SW[8:5]; // LOW: Erosion, HIGH: Dilatation
-	state s0 (reset, pixel_clock, proc_what);
+	wire reset, just_borders;
+	state s0 (clock, SW[9:7], proc_what, just_borders, reset);
 	
 	// Todos os cabos que vamos precisar
 	wire video_on, mem_clock, pixel_clock;
@@ -30,7 +28,9 @@ module top
 	// Controladora VGA
 	VGA_SYNC v0
 		( .clock_50MHz(CLOCK_50)
-		, .red(activate & red_time), .green(activate & green_time), .blue(activate & blue_time)
+		, .red(activate & red_time)
+		, .green(activate & green_time)
+		, .blue(activate & blue_time)
 		, .red_out(vga_sr), .green_out(vga_sg), .blue_out(vga_sb)
 		, .horiz_sync_out(VGA_HS), .vert_sync_out(VGA_VS)
 		, .video_on(video_on), .pixel_clock(pixel_clock)
@@ -44,7 +44,8 @@ module top
 	assign VGA_B = (vga_sb ? 255 : 0);
 	
 	// Nossos componentes
-	janela j0 (pixel_row, pixel_column, mask, red_time, green_time, blue_time, sq_right, sq_bottom);
+	janela j0 (pixel_row, pixel_column, mask, red_time, green_time,
+		blue_time, sq_right, sq_bottom);
 	conc c0 (pixel_row, pixel_column, conc_s);
 	
 	// Tratamento morfologico
@@ -53,8 +54,12 @@ module top
 	img_proc p1 (reset, mem_clock, proc_what[2], conc_s, p0_q, p1_q);
 	img_proc p2 (reset, mem_clock, proc_what[1], conc_s, p1_q, p2_q);
 	img_proc p3 (reset, mem_clock, proc_what[0], conc_s, p2_q, p3_q);
-	assign out_q = (sq_right && sq_bottom? p3_q : (sq_bottom ? p2_q : (sq_right ? p1_q : p0_q)));
-	assign LEDR = {proc_what[3], proc_what[2], proc_what[1], proc_what[0], 5'b0, reset};
+	assign out_q = (just_borders? (p0_q ^ p1_q) :
+			(sq_right && sq_bottom? p3_q :
+				(sq_bottom ? p2_q : (sq_right ? p1_q : p0_q))
+		));
+	assign LEDR = {proc_what[3], proc_what[2], proc_what[1], proc_what[0],
+		just_borders, 4'b0, reset};
 	
 	// Memoria com a Imagem Original
 	altsyncram
@@ -75,12 +80,39 @@ module top
 endmodule
 
 module state
-	( input reset, clock
-	, output [3:0] procs
+	( input clock
+	, input [2:0] sw
+	, output reg [3:0] procs
+	, output reg just_borders
+	, output reg reset
 	);
-	reg [27:0] counter;
-	assign procs = {counter[24], counter[25], counter[26], counter[27]};
 	
-	always @(posedge reset, posedge clock)
-		counter <= (reset ? 0 : ((counter+1) % {28{1'b1}}));
+	always @(sw)
+		case(sw)
+			3'b000: begin // IMAGEM ORIGINAL = RESET
+					reset <= 1;
+					procs <= 4'b0000;
+					just_borders <= 0;
+				end
+			3'b001: begin // SO EROSAO
+					reset <= 0;
+					procs <= 4'b0000;
+					just_borders <= 0;
+				end
+			3'b010: begin // SO DILATACAO
+					reset <= 0;
+					procs <= 4'b1111;
+					just_borders <= 0;
+				end
+			3'b011: begin // FILTRO
+					reset <= 0;
+					procs <= 4'b1001;
+					just_borders <= 0;
+				end
+			default: begin // BORDAS
+					reset <= 0;
+					procs <= 4'b0101;
+					just_borders <= 1;
+				end
+		endcase
 endmodule
